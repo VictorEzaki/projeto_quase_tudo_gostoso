d5b5e7e39abdb84599c392c61f548b404e81c051
victor 406 <victor.ezaki@alunos.sc.senac.br>
2025-10-06 21:40:26 -0300

    clase de exemplo para refatoração

--- Categoria.java
+++ Categoria.java
@@ -4,9 +4,9 @@ import java.util.List;
 public class Categoria {
     private static int contador = 1;
 
-    Integer id;
-    String categoria;
-    Integer ativo;
+    private Integer id;
+    private String categoria;
+    private Integer ativo;
 
     public static ArrayList<Categoria> categorias = new ArrayList<>();
     public List<ReceitaCategoria> receitaCategorias = new ArrayList<>();
@@ -15,11 +15,39 @@ public class Categoria {
         this.id = contador++;
         this.categoria = categoria;
         this.ativo = ativo;
-
-        categorias.add(this);
     }
 
     public static ArrayList<Categoria> listarCategorias() {
         return categorias;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public Integer getAtivo() {
+        return this.ativo;
+    }
+    
+    public void setAtivo(Integer ativo) {
+        this.ativo = ativo;
+    }
+
+    public String toString() {
+        return "id: " + id +
+               "\nCategoria: " + categoria +
+               "\nStatus: " + ativo;
     }
 }
